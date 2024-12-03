897d7f4a456cc9db7e3cbdd41108ad21e0daa238
Alexandre Da Silva <alexandrerider@hotmail.com>
2024-12-03 19:36:22 -0300

    putting create component to be the main route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    {path: "", component: AppComponent},
+    {path: "", loadComponent: () => import("./components/card-creator/card-creator.component").then(m => m.CardCreatorComponent)},
     {path: "form", loadComponent: () =>  import("./components/form/form.component").then(m => m.FormComponent)},
     {path: "card", loadComponent: () =>  import("./components/card/card.component").then(m => m.CardComponent)},
     {path: "create", loadComponent: () => import("./components/card-creator/card-creator.component").then(m => m.CardCreatorComponent)}
