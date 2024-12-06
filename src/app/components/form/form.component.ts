345c0388d86d93203c5323cd8f75834424e1c8bc
Alexandre Da Silva <alexandrerider@hotmail.com>
2024-12-05 21:35:55 -0300

    Adding image picker input, image picker url dependency: https://github.com/josealejandro2928/ngp-image-picker

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -5,10 +5,16 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { CardModel } from '../../interfaces/card-model';
 import html2canvas from 'html2canvas';
+import { ImagePickerConf, NgpImagePickerModule } from 'ngp-image-picker';
 
 @Component({
   selector: 'app-form',
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule,FormsModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    NgpImagePickerModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
@@ -16,17 +22,15 @@ export class FormComponent {
   @Output() enviandoCard = new EventEmitter<CardModel>();
   card:CardModel = {name:"",vida:0,atk:0, def:0, image:null, especialName:"",especial:""};
   @Input() element:any;
-
-  onImageUpload(event:Event) {
-    let input = event.target as HTMLInputElement;
-    if(input?.files && input.files[0]) {
-      let file = input.files[0];
-      let reader = new FileReader();
-      reader.onload = () => {
-        this.card.image = reader.result;
-      }
-      reader.readAsDataURL(file);
-    }
+  imagePickerConfig: ImagePickerConf = {
+    borderRadius: "4px",
+    language: "en",
+    width: "320px",
+    height: "240px"
+  }
+  
+  onImageChanged(dataUri:ArrayBuffer | string | null) {
+    this.card.image = dataUri;
   }
 
   async downloadElementAsPng(){
